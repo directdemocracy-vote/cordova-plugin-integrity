8ed565e3ff87730d8024d82ede745e2e131811c3
megatol <74714212+megatol@users.noreply.github.com>
2023-10-16 16:23:18 +0200

    Update Integrity.java

--- src/android/Integrity.java
+++ src/android/Integrity.java
@@ -5,25 +5,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class Integrity extends CordovaPlugin {
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-        String alias = data.getString(0);
-        if (action.equals("greet")) {
-          String message = "Hello " + alias;
-          callbackContext.success(message);
-          return true;
-        } else if (action.equals("check")) {
-          try {
-            return check(alias, callbackContext);
-          } catch (Exception e) {
-            return false;
-          }
-        } else {
-            return false;
-        }
-    }
-
-    public boolean check(String alias, byte[] message, CallbackContext callbackContext) throws Exception {
+  @Override
+  public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    String alias = data.getString(0);
+    if (action.equals("greet")) {
+      String message = "Hello " + alias;
+      callbackContext.success(message);
       return true;
-    }
+    } else if (action.equals("check")) {
+      try {
+        return check(alias, callbackContext);
+      } catch (Exception e) {
+        return false;
+      }
+    } else
+      return false;
+  }
+  public boolean check(String alias, byte[] message, CallbackContext callbackContext) throws Exception {
+    callbackContext.success("Check done");
+    return true;
+  }
 }

79ff488e5c6eb2c0d860a95e5b09b0d46249bc98
megatol <74714212+megatol@users.noreply.github.com>
2023-10-16 16:21:33 +0200

    Update integrity.js

--- www/integrity.js
+++ www/integrity.js
@@ -1,6 +1,9 @@
 /* global cordova, module */
 
 module.exports = {
+  greet: function (name, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Integrity", "greet", [name]);
+  },
   check: function (name, successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "Integrity", "check", [name]);
   }

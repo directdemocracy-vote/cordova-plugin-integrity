37c38dea0434376a3457aa833733af92c41c6000
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-06-28 07:36:28 +0200

    Updated to new Google API

--- src/android/Integrity.java
+++ src/android/Integrity.java
@@ -2,7 +2,6 @@ package vote.directdemocracy.integrity;
 
 import android.content.Context;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailabilityLight;
 import com.google.android.gms.tasks.Task;
 
@@ -17,17 +16,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class Integrity extends CordovaPlugin {
-  @Override public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+  @Override
+  public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
     if (action.equals("check")) {
       String nonce = data.getString(0);
       Context context = this.cordova.getActivity().getApplicationContext();
-      if (GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(context) != ConnectionResult.SUCCESS) {
+      if (GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(context) != 0) {
         callbackContext.error("Play Services not found");
         return true;
       }
       IntegrityManager integrityManager = IntegrityManagerFactory.create(context);
       Task<IntegrityTokenResponse> integrityTokenResponse = integrityManager.requestIntegrityToken(
-        IntegrityTokenRequest.builder().setNonce(nonce).build());
+          IntegrityTokenRequest.builder().setNonce(nonce).build());
       integrityTokenResponse.addOnSuccessListener(integrityTokenResponse1 -> {
         String integrityToken = integrityTokenResponse1.token();
         callbackContext.success(integrityToken);
